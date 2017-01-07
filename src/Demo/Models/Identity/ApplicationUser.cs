6d864924167c7ddb8e9426d0bb86f6f746c86b4f
AspNetCP <key.man@gmx.com>
2017-01-07 22:06:56 +0800

    ApplicationUser ApplicationDbContext

--- src/Demo/Models/Identity/ApplicationUser.cs
+++ src/Demo/Models/Identity/ApplicationUser.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Demo.Models.Identity
 {
-    public class ApplicationUser
+    public class ApplicationUser:IdentityUser
     {
     }
 }
